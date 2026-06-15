8f92bc38e38010d90fdef7b1bb8a91a893487bad
ssejangkung227-creator <ssejangkung227@gmail.com>
2026-06-15 10:12:44 +0700

    akun.php

--- akun.php
+++ akun.php
@@ -37,7 +37,7 @@ if (isset($_POST['update_password'])) {
 <head>
 <meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Akun Saya - EOQ Sistem</title>
-<link rel="stylesheet" href="../style.css">
+<link rel="stylesheet" href="style.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body>
